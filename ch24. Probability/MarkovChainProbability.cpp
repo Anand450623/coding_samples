a7de26a4506d569971b7260e00125b1ee31daa08
Anand450623 <ak450623@gmail.com>
2020-07-24 02:32:36 +0530

    markov chain probability

---
+++ ch24. Probability/MarkovChainProbability.cpp
@@ -0,0 +1,101 @@
+/*============================================================================
+ * Problem : Given a Markov chain G, we have the find the probability of reaching the state F at time t = T
+ * 			 if we start from state S at time t = 0.
+ *
+ * Author  : Anand Kumar
+============================================================================*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+# define N 6
+
+/*
+ * A Markov chain is a random process consisting of various states and the probabilities of moving from one state to another.
+ * We can represent it using a directed graph where the nodes represent the states and the edges represent the probability of
+ * going from one node to another. It takes unit time to move from one node to another.
+ *
+ * The sum of the associated probabilities of the outgoing edges is one for every node.
+ */
+
+void multiply(float src[N][N], float dest[N][N])
+{
+
+	float temp[N][N];
+	memset(temp, 0, sizeof(temp));
+	for(int  i=0; i<N; i++)
+	{
+		for(int j=0; j<N; j++)
+		{
+			for(int k=0; k<N; k++)
+			{
+				temp[i][j] += src[i][k]*dest[k][j];
+			}
+		}
+	}
+
+	for(int  i=0; i<N; i++)
+		for(int j=0; j<N; j++)
+			src[i][j] = temp[i][j];
+
+}
+
+void power(float probability[N][N], int n)
+{
+
+	if(n==0 || n==1)
+		return;
+
+	float copy[N][N];
+
+	for(int  i=0; i<N; i++)
+		for(int j=0; j<N; j++)
+			copy[i][j] = probability[i][j];
+
+	power(probability, n/2);
+
+	multiply(probability, probability);
+
+	if(n%2)
+		multiply(probability, copy);
+
+}
+
+int main()
+{
+
+	int s = 4; //s is the starting point.
+	int t = 100; //t is the number of time units.
+
+	float probability[N][N] = {{ 0, 0.09, 0, 0, 0, 0 },
+						   	   { 0.23, 0, 0, 0, 0, 0.62 },
+							   { 0, 0.06, 0, 0, 0, 0 },
+							   { 0.77, 0, 0.63, 0, 0, 0 },
+							   { 0, 0, 0, 0.65, 0, 0.38 },
+							   { 0, 0.85, 0.37, 0.35, 1.0, 0 }};
+
+	power(probability, 100);
+	float entryMat[N] = {0};
+	entryMat[s-1] = 1;
+
+	float temp[N];
+
+	for(int i=0; i<N; i++)
+	{
+		float sum = 0.0f;
+		for(int j=0; j<N; j++)
+		{
+			sum += probability[i][j] * entryMat[j];
+		}
+		temp[i] = sum;
+	}
+
+	cout<<"The probabilities of being at ith state after T = "<<t<<" time unit is given by:::\n";
+
+	for(int i=0; i<N; i++)
+		cout<<temp[i]<<" ";
+	cout<<endl;
+
+	return 0;
+
+}
--- ch24. Probability/README.md
+++ ch24. Probability/README.md
@@ -1,1 +1,19 @@
 This repository contains algorithm implementation for various probability related problems.
+
+Markov chain dynamic programming approach:
+
+We can use dynamic programming and depth-first search (DFS) to solve this problem, 
+by taking the state and the time as the two DP variables. 
+
+We can easily observe that the probability of going from state A to state B at time t is equal to the 
+product of the probability of being at A at time t - 1 and the probability associated with the edge 
+connecting A and B. 
+
+Therefore the probability of being at B at time t is the sum of this quantity for all A adjacent to B.
+
+For eg. In our 2-D array, row can act as state and column can act as time.
+so, P[B][T]  = P[A][T-1]* P[A][B]
+
+This P[B][T] has to be summmed upon for all the adjacent states of B.
+
+Reference link: https://www.geeksforgeeks.org/find-the-probability-of-a-state-at-a-given-time-in-a-markov-chain-set-1/?ref=lbp
